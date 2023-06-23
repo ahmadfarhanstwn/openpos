4fd85c9d1ee3be60f53a4f9442555eae16ba987d
Ahmad Farhan <ahmadfarhanstwn@gmail.com>
2023-06-23 23:59:45 +0700

    create product table migrations

---
+++ server/database/migrations/2023_06_20_150158_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id('product_id');
+            $table->string('product_barcode', 25);
+            $table->string('product_name', 50);
+            $table->integer('unit_id');
+            $table->integer('category_id');
+            $table->float('unit_in_stock');
+            $table->float('product_price');
+            $table->float('discount_percentage');
+            $table->integer('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
